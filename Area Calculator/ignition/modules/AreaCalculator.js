b8a87c0f400843ce2f473d3f1651d204ebefcd1a
frankie-powers <franklin.power21@gmaul.com>
2024-09-12 19:00:54 +0100

    deleted

--- Area Calculator/ignition/modules/AreaCalculator.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("areaCalculatorModule", (m) => {
-  const AreaCalculator = m.contract("areaCalculator");
-
-  return { AreaCalculator };
-});
--- NFT2.0 SVG SMART CONTRACT/ignition/modules/NFT2.0.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const NFTModule = buildModule("NFTModule", (m) => {
-  const NFT = m.contract("SVGNFT");
-
-  return { NFT };
-});
-
-export default NFTModule;

cf61076b7e44836a6fe47ba17a3367073baaf9c6
frankie-powers <franklin.power21@gmaul.com>
2024-09-12 23:02:25 +0100

    testing

--- 2. Area Calculator/test/calculator.ts
+++ 2. Area Calculator/test/calculator.ts
@@ -1,4 +1,20 @@
 import { expect } from "chai";
-import hre from "hardhat";
+import { ethers } from "hardhat";
+import { Await } from "react-router-dom";
 
-describe();
+describe("AreaCalculator", function () {
+  let AreaCalculator;
+  let areaCalculator;
+  let owner;
+
+  beforeEach(async function () {
+    // Deploy the smart contract before each test
+    AreaCalculator = await ethers.getContractFactory("areaCalculator");
+    owner = await AreaCalculator.deploy();
+    await areaCalculator.deployed();
+  });
+});
+
+describe("Area of a Triangle", function () {
+  it("should calculate the area of a triangle", async function (params: type) {});
+});

ff9da9c89b93ecbbdb59cffd94847e6fc94547db
frankie-powers <franklin.power21@gmaul.com>
2024-09-13 03:41:50 +0100

    updates  to assist you

--- 3. INBOX/README.md
+++ 3. INBOX/README.md
@@ -11,3 +11,14 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.js
 ```
+
+Deployed Addresses
+
+inboxmodule#Inbox - 0x26c5E1D15c8AcDEBD7798B25092b5e9Dfb1f2596
+
+Verifying deployed contracts
+
+Verifying contract "contracts/Inbox.sol:Inbox" for network lisk-sepolia...
+Successfully verified contract "contracts/Inbox.sol:Inbox" for network lisk-sepolia:
+
+- https://sepolia-blockscout.lisk.com//address/0x26c5E1D15c8AcDEBD7798B25092b5e9Dfb1f2596#code
--- 3. INBOX/hardhat.config.js
+++ 3. INBOX/hardhat.config.js
@@ -1,15 +1,34 @@
-require("@nomicfoundation/hardhat-ignition");
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
-  // other configurations
   networks: {
-    sepolia: {
-      url: "https://rpc.sepolia-api.lisk.com",
-      accounts: [
-        "0xa20ca6c555f231c801e23f0d359a24cb7d3637c51cb02c5b3be75faef8e4dee3",
-      ],
+    // LISK
+    "lisk-sepolia": {
+      url: process.env.LISK_RPC_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+      gasPrice: 1000000000,
     },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "123",
+    },
+    customChains: [
+      {
+        network: "lisk-sepolia",
+        chainId: 4202,
+        urls: {
+          apiURL: "https://sepolia-blockscout.lisk.com/api",
+          browserURL: "https://sepolia-blockscout.lisk.com/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
   },
 };

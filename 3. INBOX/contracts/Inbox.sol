bf4ba0b246adaaf5decb59203427baf81196c26a
frankie-powers <franklin.power21@gmaul.com>
2024-09-13 09:51:36 +0100

    comments

--- 3. INBOX/contracts/Inbox.sol
+++ 3. INBOX/contracts/Inbox.sol
@@ -3,12 +3,16 @@
 pragma solidity 0.8.4;
 
 contract Inbox {
+
+    //State Variables
     string public message;
     
+    // Initiate default message at constructor
     constructor(string memory initialMessage) {
         message = initialMessage;
     }
     
+    // Update the message variable with a new message.
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }

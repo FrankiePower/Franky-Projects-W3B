e3c3445484ad69b9b7a25ddfa4d3801194aa982d
frankie-powers <franklin.power21@gmaul.com>
2024-09-13 11:17:02 +0100

    deleted files

--- 4. STORAGE/ignition/modules/Storage.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("storagemodule", (m) => {
-  const storageContract = m.contract("Storage");
-
-  return { storageContract };
-});

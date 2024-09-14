0f67790b375137c29c2b8812772b26eaa14abe76
frankie-powers <franklin.power21@gmaul.com>
2024-09-14 04:30:14 +0100

    removed anyvalue

--- 6. STUDENT REGISTRATION/test/registration-test.js
+++ 6. STUDENT REGISTRATION/test/registration-test.js
@@ -1,5 +1,4 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
